3182c5e9f9b0e5e060812fff50458699a6498c4d
Cássio Alves Silva <cassio.alves09@gmail.com>
2022-11-06 10:11:46 -0300

    project

---
+++ src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <main class="columns is-gapless is-multiline" :class="{ 'modo-escuro': modoEscuroAtivo}">
+    <div class="column is-one-quarter">
+      <BarraLateral @aoTemaAlterado="trocarTema"/>
+    </div>
+    <div class="column is-three-quarter conteudo">
+      <router-view></router-view>
+    </div>
+  </main>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import BarraLateral from './components/BarraLateral.vue'
+import ITarefa from './interfaces/ITarefa'
+
+export default defineComponent({
+  components: {
+    BarraLateral
+  },
+  data(){
+    return {
+      tarefas: [] as ITarefa[],
+      modoEscuroAtivo: false
+    }
+  },
+  computed: {
+    listaVazia(): boolean{
+      return this.tarefas.length == 0;
+    }
+  },
+  methods: {
+    salvarTarefa(tarefa: ITarefa){
+      this.tarefas.push(tarefa)
+    },
+    trocarTema(modoEscuroAtivo: boolean){
+      this.modoEscuroAtivo= modoEscuroAtivo;
+    }
+  }
+});
+</script>
+
+<style>
+.lista {
+  padding: 1.25rem;
+}
+main {
+  --bg-primario: #fff;
+  --texto-primario: #000;
+}
+main.modo-escuro{
+  --bg-primario: #2b2d42;
+  --texto-primario: #ddd;
+}
+.conteudo{
+  background-color: var(--bg-primario);
+}
+</style>
