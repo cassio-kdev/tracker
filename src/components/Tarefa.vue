ca5c515450bc1debac73090fee36c2c547890948
Cássio Alves Silva <cassio.alves09@gmail.com>
2022-12-01 19:15:51 -0300

    criando modal para editar a tarefa selecionada

--- src/components/Tarefa.vue
+++ src/components/Tarefa.vue
@@ -1,6 +1,6 @@
 <template>
     <Box>
-        <div class="columns">
+        <div class="columns clicavel" @click="tarefaClicada">
             <div class="column is-4">
                 {{tarefa.descricao || 'Tarefa sem descrição!'}}
             </div>
@@ -20,6 +20,7 @@ import ITarefa from '../interfaces/ITarefa'
 import Box from './Box.vue'
 export default defineComponent({
     name: 'TarefA',
+    emits:['aoTarefaClicada'],
     components: {
         Cronometro,
         Box
@@ -29,6 +30,16 @@ export default defineComponent({
             type: Object as PropType<ITarefa>,
             required: true
         }
+    },
+    methods:{
+        tarefaClicada() : void{
+            this.$emit('aoTarefaClicada', this.tarefa)
+        }
     }
 })
 </script>
+<style scoped>
+.clicavel{
+    cursor: pointer;
+}
+</style>
